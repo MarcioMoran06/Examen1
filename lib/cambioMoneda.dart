6493880b40f0f0ece3c27c37d3a3e4bf323c6fcf
Marcio Moran <marciomoran00@gmail.com>
2023-11-12 17:13:06 -0600

    Cambio moneda sin terminar

---
+++ lib/cambioMoneda.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class CambioMonedaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cambio de monedas'),
+      ),
+      body: Center(
+        child: Text('Esta es la pagina de cambio de monedas'),
+      ),
+    );
+  }
+}
